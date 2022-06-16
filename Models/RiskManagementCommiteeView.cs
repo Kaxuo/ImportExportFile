11fd96e123b905969a9f052f6f18664d30b16b70
Hoang Nguyen <hoang.nguyen@busi.eu>
2022-06-16 17:22:41 +0200

    design in progress

---
+++ Models/RiskManagementCommiteeView.cs
@@ -0,0 +1,36 @@
+﻿namespace ExportedPDF.Models
+{
+    public class RiskManagementCommiteeView
+    {
+        public RiskManagementCommiteeView(string emergenceDate, string issueDetectedDate, string riskEventBased, string processLevel1, string processLevel2, string midiLinkedIncident, string errorType, string eventDescription, string compensationCalculationDetails, string amountIncidentInCurrency, string currency, string amount, string causeType)
+        {
+            EmergenceDate = emergenceDate;
+            IssueDetectedDate = issueDetectedDate;
+            RiskEventBased = riskEventBased;
+            ProcessLevel1 = processLevel1;
+            ProcessLevel2 = processLevel2;
+            MIDILinkedIncident = midiLinkedIncident;
+            ErrorType = errorType;
+            EventDescription = eventDescription;
+            CompensationCalculationDetails = compensationCalculationDetails;
+            AmountIncidentInCurrency = amountIncidentInCurrency;
+            Currency = currency;
+            Amount = amount;
+            CauseType = causeType;
+        }
+        public string EmergenceDate { get; set; }
+        public string IssueDetectedDate { get; set; }
+        public string RiskEventBased { get; set; }
+        public string ProcessLevel1 { get; set; }
+        public string ProcessLevel2 { get; set; }
+        public string MIDILinkedIncident { get; set; }
+        public string ErrorType { get; set; }
+        public string EventDescription { get; set; }
+        public string CompensationCalculationDetails { get; set; }
+        public string AmountIncidentInCurrency { get; set; }
+        public string Currency { get; set; }
+        public string Amount { get; set; }
+        public string CauseType { get; set; }
+
+    }
+}
--- Repository/ExportFileRepository.cs
+++ Repository/ExportFileRepository.cs
@@ -15,7 +15,7 @@ namespace ExportPDF.Repository
             // ExportedPDF.dll can be found in bin/debug/.. , needed to find the rdlc file
             string fileDirPath = Assembly.GetExecutingAssembly().Location.Replace("ExportedPDF.dll", string.Empty);
             // Providing the path and file name
-            string rdlcFilePath = string.Format("{0}ReportFiles\\{1}.rdlc", fileDirPath, "ProductDetails");
+            string rdlcFilePath = string.Format("{0}ReportFiles\\{1}.rdlc", fileDirPath, "ActionPlanReport");
             // Create instance of System Text Encoding 
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             Encoding.GetEncoding("utf-8");
@@ -23,9 +23,9 @@ namespace ExportPDF.Repository
             LocalReport report = new LocalReport(rdlcFilePath);
             // Simulate data for the report
             List<ProductDto> productList = new List<ProductDto> {
-                { new ProductDto("jptyhjtytyjty", "Blob", "email", "09790709") }, { new ProductDto("Kain", "Dragoon", "email", "09790709") }, { new ProductDto("Cecil", "DK", "email", "09790709") }, { new ProductDto("Vivi", "BLM", "email", "09790709") } };
+                { new ProductDto("jptyhjtytyjty", "Blob", "email", "09790709") }};
             // dsProducts needs to match the datasets of the rdlc file, the properties of the model should also matched the fields of the datasets
-            report.AddDataSource("dsProducts", productList);
+            report.AddDataSource("dsDatasets", productList);
             Dictionary<string, string> parameters = new Dictionary<string, string>();
             // Create render type , pdf, excel or any other
             var result = report.Execute(RenderType.Pdf, 1, parameters);
